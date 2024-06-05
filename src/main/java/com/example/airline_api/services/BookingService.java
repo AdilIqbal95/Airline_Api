09845d66c82cf9a63aa6d62e7d19ecfe4436c1d8
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-06-05 18:39:26 +0100

    added getAll, getById and save methods to flight, passenger model and booking(except save)

--- src/main/java/com/example/airline_api/services/BookingService.java
+++ src/main/java/com/example/airline_api/services/BookingService.java
@@ -1,13 +1,39 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Booking;
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.BookingRepository;
+import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookingService {
 
     @Autowired
     BookingRepository bookingRepository;
+
+    @Autowired
+    PassengerService passengerService;
+
+    @Autowired
+    FlightService flightService;
+
+    public List<Booking> getAllPassengers() {
+        return bookingRepository.findAll();
+    }
+
+    public Optional<Booking> getPassengerById(long id) {
+        return bookingRepository.findById(id);
+    }
+
+    public void deleteBooking(long id) {
+        bookingRepository.deleteById(id);
+    }
+
+//    TODO - saveBooking
 
 }
